fc19afba2165d9abfa6f2d4b622056fc5348088d
Shahzaneer Ahmed <shahism01@gmail.com>
2022-04-06 15:35:05 +0500

    added 💫

--- python.py
+++ python.py
@@ -3,3 +3,6 @@ print('Shahzaneer Ahmed here')
 print('Lam yaaati nazeero kafi nazarin mislay tou na shud paidaa jaaanaaa')
 
 print('jag raaj ko taj torayy sar so haiii tujhe shah e dosraaa janaaaaa')
+
+
+print("Pakistan ")
