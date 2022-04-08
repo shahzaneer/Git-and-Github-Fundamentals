bb9f6ee556a06a8f32ea7e1576b3203c7fecc4ef
Shahzaneer Ahmed <shahism01@gmail.com>
2022-04-08 23:26:50 +0500

    rm -rf .git command to delete the git

--- python.py
+++
@@ -1,8 +0,0 @@
-print('Shahzaneer Ahmed here')
-
-print('Lam yaaati nazeero kafi nazarin mislay tou na shud paidaa jaaanaaa')
-
-print('jag raaj ko taj torayy sar so haiii tujhe shah e dosraaa janaaaaa')
-
-
-print("Pakistan ")
