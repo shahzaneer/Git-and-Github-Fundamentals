a9de9481c73b8d3f80b871e9e61cb6176f24ffd9
Shahzaneer Ahmed <shahism01@gmail.com>
2022-04-09 01:43:36 +0500

    naat added meri ulfat madeenay se yunhi nhi

--- shah.py
+++ shah.py
@@ -1,3 +1,12 @@
 print("This is Shahzaneer Ahmed taking on GITHUB \n")
 
-print("rm -rf .git")  
+print("rm -rf .git") 
+
+print("MERI ULFAT MADEENAY SE YUNHI NHI ! MERA DEEN AUR DUNYA MADEENAY MAIN HAI!")
+print("""Mein Madine ki jaanib na kaise khinchu
+mera deen aur dunya madine mein hai""")
+
+print("""phir mujhe maut ka koi khatra na ho
+maut kya zindagi ki bhi parwa na ho
+kaash sarkar eik baar mujh se kahein
+ab tera marna jeena Madine mein hai""")
