7a9e5e5de2510634e16780cb5d8f407affecc2f0
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-04-09 17:27:51 +0500

    delete Main.java

--- Main.java
+++
@@ -1,5 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("Sahib e rija't e shams shaq ul qamar! naib e dast e gudrat per lakhon salaam !");
-    }
-}
